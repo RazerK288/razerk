792e234ff6ccb91e358af1e1310d7632906f85e7
RazerK288 <keithanoxendine26@gmail.com>
2025-06-14 18:49:15 -0400

    Add files via upload

---
+++ map.lua
@@ -0,0 +1,23 @@
+--------------------------------------------------
+-- Episode code for every level
+-- Created 15:6 2025-6-11
+--------------------------------------------------
+
+-- Run code on the first frame
+function onStart()
+    --Your code here
+end
+
+-- Run code every frame (~1/65 second)
+-- (code will be executed before game logic will be processed)
+function onTick()
+    --Your code here
+end
+
+-- Run code when internal event of the SMBX Engine has been triggered
+-- eventName - name of triggered event
+function onEvent(eventName)
+    --Your code here
+end
+
+
